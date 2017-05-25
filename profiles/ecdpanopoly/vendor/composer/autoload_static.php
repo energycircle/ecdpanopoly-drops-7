05bcbc139fb338b2b4d8ce940fb90c56d86253d5
Thomas P. Scola Jr <thomas@energycircle.com  git config --global user.name Thomas Scola>
2017-05-25 12:03:22 -0400

    update composer

--- profiles/ecdpanopoly/vendor/composer/autoload_static.php
+++ profiles/ecdpanopoly/vendor/composer/autoload_static.php
@@ -4,10 +4,13 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInitc1d4d45596d87e7fb5d8b2149620fabc
+class ComposerStaticInitb7432f28cf9095908d1d0195ba8a22cf
 {
     public static $files = array (
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
+        'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
+        'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
+        '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
     );
 
     public static $prefixLengthsPsr4 = array (
@@ -31,6 +34,17 @@ class ComposerStaticInitc1d4d45596d87e7fb5d8b2149620fabc
         'P' => 
         array (
             'Psr\\Log\\' => 8,
+            'Psr\\Http\\Message\\' => 17,
+        ),
+        'G' => 
+        array (
+            'GuzzleHttp\\Psr7\\' => 16,
+            'GuzzleHttp\\Promise\\' => 19,
+            'GuzzleHttp\\' => 11,
+        ),
+        'D' => 
+        array (
+            'DrupalComposer\\DrupalScaffold\\' => 30,
         ),
     );
 
@@ -95,18 +109,46 @@ class ComposerStaticInitc1d4d45596d87e7fb5d8b2149620fabc
         array (
             0 => __DIR__ . '/..' . '/psr/log/Psr/Log',
         ),
+        'Psr\\Http\\Message\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-message/src',
+        ),
+        'GuzzleHttp\\Psr7\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/psr7/src',
+        ),
+        'GuzzleHttp\\Promise\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/promises/src',
+        ),
+        'GuzzleHttp\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/guzzle/src',
+        ),
+        'DrupalComposer\\DrupalScaffold\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/drupal-composer/drupal-scaffold/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
         'G' => 
         array (
-            'Guzzle\\Tests' => 
+            'Guzzle\\Stream' => 
             array (
-                0 => __DIR__ . '/..' . '/guzzle/guzzle/tests',
+                0 => __DIR__ . '/..' . '/guzzle/stream',
             ),
-            'Guzzle' => 
+            'Guzzle\\Parser' => 
             array (
-                0 => __DIR__ . '/..' . '/guzzle/guzzle/src',
+                0 => __DIR__ . '/..' . '/guzzle/parser',
+            ),
+            'Guzzle\\Http' => 
+            array (
+                0 => __DIR__ . '/..' . '/guzzle/http',
+            ),
+            'Guzzle\\Common' => 
+            array (
+                0 => __DIR__ . '/..' . '/guzzle/common',
             ),
             'Goutte' => 
             array (
@@ -142,9 +184,9 @@ class ComposerStaticInitc1d4d45596d87e7fb5d8b2149620fabc
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInitc1d4d45596d87e7fb5d8b2149620fabc::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInitc1d4d45596d87e7fb5d8b2149620fabc::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInitc1d4d45596d87e7fb5d8b2149620fabc::$prefixesPsr0;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitb7432f28cf9095908d1d0195ba8a22cf::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitb7432f28cf9095908d1d0195ba8a22cf::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInitb7432f28cf9095908d1d0195ba8a22cf::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
