694a4da18fe6755d9d511862d71e9ad30260cba7
Thomas P. Scola Jr <thomas@energycircle.com  git config --global user.name Thomas Scola>
2017-06-16 22:41:24 -0400

    update composer one more time

--- profiles/ecdpanopoly/libraries/mailchimp/vendor/autoload.php
+++ profiles/ecdpanopoly/libraries/mailchimp/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit716264d799c084744e34a98243f9b34b::getLoader();
+return ComposerAutoloaderInit01692dc58bf3d74ebf171bcd376b927b::getLoader();
